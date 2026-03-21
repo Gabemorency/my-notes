e869628dcfb81cac7e175d3da0afaf23d55e018a
Gabriel Morency <gabrielmorency@mac.mynetworksettings.com>
2026-03-21 15:56:21 -0400

    Quartz sync: Mar 21, 2026, 3:56 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,8 +31,8 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.TableOfContents(),
     Component.Graph({
-      localGraph: { fontSize: 0.2, linkDistance: 60 },
-      globalGraph: { fontSize: 0.2, linkDistance: 60 },
+      localGraph: { fontSize: 0.32, linkDistance: 60 },
+      globalGraph: { fontSize: 0.32, linkDistance: 60 },
     }),
     Component.Backlinks(),
   ],
@@ -54,8 +54,8 @@ export const defaultListPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph({
-      localGraph: { fontSize: 0.2, linkDistance: 60 },
-      globalGraph: { fontSize: 0.2, linkDistance: 60 },
+      localGraph: { fontSize: 0.32, linkDistance: 60 },
+      globalGraph: { fontSize: 0.32, linkDistance: 60 },
     }),
   ],
 }
